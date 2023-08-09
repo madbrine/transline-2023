d680e438998a0fbbdc9da5271aa50b6bbdc3f818
Pavel Stepanov <115621772+madbrine@users.noreply.github.com>
2023-08-09 09:44:39 +0600

    upd

--- src/compnents/our-services/index.js
+++ src/compnents/our-services/index.js
@@ -1,26 +1,66 @@
 import MoBlockHeader from "@/molecules/block-header";
 import MoBlockLine from "@/molecules/block-line";
 import s from './styles.module.css';
+import { motion, useScroll } from "framer-motion";
+import { useRef, useState } from "react";
 
 function CoOurServices() {
-    return(
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["end end", "start start"]
+    });
+    const [IsAnimate, setAnimate] = useState(0);
+    useState(() => {
+        let newValud = 1000 * scrollYProgress;
+        setAnimate(scrollYProgress);
+        console.log(IsAnimate);
+    }, [scrollYProgress]);
+    return (
         <div>
             <MoBlockLine text="Услуги" />
-            <MoBlockHeader text="Наши услуги" getFormButton/>
+            <MoBlockHeader
+                text="Решения, для компаний, любой сложности"
+                small
+                getFormButton
+            />
             <div className={s['container']}>
-                <div className={s['wrap-container']}>
-                    <div className={s['wrap-item-white']}></div>
-                    <div className={s['wrap-item']}></div>
-                    <div className={s['wrap-item']}></div>
-                    <div className={s['wrap-item']}></div>
-                    <div className={s['wrap-item']}></div>
-                    <div className={s['wrap-item']}></div>
-                    <div className={s['wrap-item']}></div>
-                    <div className={s['wrap-item-white']}></div>
-                    <div className={s['wrap-item']}></div>
-                    <div className={s['wrap-item']}></div>
-                    <div className={s['wrap-item-white']}></div>
-                    <div className={s['wrap-item-white']}></div>
+                <div
+                ref={ref}
+                >
+                    <div
+                        style={{
+                            marginLeft: 0,
+                        }}
+                    >
+                        <div className={s['services-container']}>
+                            <motion.div className={s['service']}
+                                style={{
+                                    border: 'solid',
+                                    borderWidth: {IsAnimate},
+                                    borderColor: '#000'
+                                }}
+                            >
+                                <div className={s['service-image']}></div>
+                            </motion.div>
+                            <div className={s['service']}>
+                                <div className={s['service-image']}></div>
+                            </div>
+                            <div className={s['service']}>
+                                <div className={s['service-image']}></div>
+                            </div>
+                            <div className={s['service']}>
+                                <div className={s['service-image']}></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div style={{
+                    height: 710
+                }}>
+                    <motion.div>
+                        {scrollYProgress}
+                    </motion.div>
                 </div>
             </div>
         </div>
--- src/molecules/block-header/index.js
+++ src/molecules/block-header/index.js
@@ -12,13 +12,14 @@ const inter = Inter({
 function MoBlockHeader(props) {
     return (
         <div
-        style={{
-            display: 'flex',
-            marginLeft: '60px',
-            marginRight: '60px',
-            justifyContent: 'space-between',
-            alignItems: 'flex-end',
-        }}>
+            style={{
+                display: 'flex',
+                marginLeft: '60px',
+                marginRight: '60px',
+                justifyContent: 'space-between',
+                alignItems: 'flex-end',
+            }}>
+            {!props.small ?
             <h1 
                 className={inter.className}
                 style={{
@@ -28,6 +29,19 @@ function MoBlockHeader(props) {
             >
                 {props.text}
             </h1>
+            :
+            <h1 
+                className={inter.className}
+                style={{
+                    fontSize: '48px',
+                    letterSpacing: '-1,92px',
+                    lineHeight: '110%',
+                    margin: '0 0 0 0'
+                }}
+            >
+                {props.text}
+            </h1>
+            }
             {props.getFormButton &&
                 <div style={{
                     border: 'solid 1px #999',
