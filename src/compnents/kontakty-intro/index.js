ac197a17205a1bee5c255d978a50d25a0b22cce7
Deduh <deduh@email.com>
2023-08-10 10:29:35 +0600

    upd mobile adapt
    kontakty

--- src/compnents/kontakty-intro/index.js
+++ src/compnents/kontakty-intro/index.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import s from './styles.module.css';
+import MoNavLinker from "@/molecules/nav-linker";
+import MoBlockHeader from "@/molecules/block-header";
 
 const inter = Inter({
     subsets: ['latin'],
@@ -8,34 +10,30 @@ const inter = Inter({
 
 function KontaktyIntro() {
     return (
-        <div className={s['container']}>
-            <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'flex-end',
-                alignItems: 'flex-end',
-            }}
-            className={inter.className}
-            >
-                <div className={s['body']}>
-                    <div style={{display: 'flex'}}>
-                        <div className={s['column']}>
-                            <a>Контакты:</a>
-                            <div style={{marginTop: 32}}>+7 (727)-367-11-04</div>
-                            <div>info@transline.kz</div>
-                        </div>
-                        <div className={s['column']}>
-                            <a>Соц. сети</a>
-                            <div style={{marginTop: 32}}>
-                                Facebook
+        <div className={s['margin-block']}>
+            <MoBlockHeader text="Контакты" />
+            <MoNavLinker text2="Контакты" />
+            <div className={s['container']}>
+                <div id={s['block-pos']} className={inter.className}>
+                    <div className={s['body']}>
+                        <div style={{ display: 'flex' }}>
+                            <div className={s['column']}>
+                                <a>Контакты:</a>
+                                <div id={s['margin-top']}>+7 (727)-367-11-04</div>
+                                <div>info@transline.kz</div>
                             </div>
-                            <div>Twitter</div>
+                            <div className={s['column']}>
+                                <a>Соц. сети</a>
+                                <div id={s['margin-top']}>
+                                    Facebook
+                                </div>
+                                <div>Twitter</div>
+                            </div>
                         </div>
                     </div>
+                    <div className={s['line']} />
+                    <div className={s['map']} />
                 </div>
-                <div className={s['line']}/>
-                <div className={s['map']}/>
             </div>
         </div>
     )
