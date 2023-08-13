428c62e01c052b0c63bb306b9b5ae2f20ff0f70b
Pavel Stepanov <115621772+madbrine@users.noreply.github.com>
2023-08-14 03:41:20 +0600

    upd

---
+++ src/compnents/application-modal/index.js
@@ -0,0 +1,14 @@
+import CoSubmitApplication from '../submit-application';
+import s from './styles.module.css'
+
+function CoApplicationModal() {
+    return (
+        <>
+            <div classNam={s['container']}>
+            </div>
+            <div className={s['background']}>
+            </div>
+        </>
+    )
+}
+export default CoApplicationModal;
