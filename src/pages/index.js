ca128e7120311cb093d497123e1048e5e5998669
Pavel Stepanov <115621772+madbrine@users.noreply.github.com>
2023-08-17 16:53:08 +0600

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main style={{ overflowX: 'hidden' }}>
+      <main style={{ overflowX: 'hidden', width: '100vw'}}>
         <CoHeader />
         <CoIntro />
         <VanishDiv>
