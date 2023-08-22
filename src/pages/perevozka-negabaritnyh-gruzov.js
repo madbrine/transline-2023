1683c22b2f054e03ea11fdaefe0d5aaf9c59a2c1
Pavel Stepanov <115621772+madbrine@users.noreply.github.com>
2023-08-22 15:20:02 +0600

    upd

--- src/pages/perevozka-negabaritnyh-gruzov.js
+++ src/pages/perevozka-negabaritnyh-gruzov.js
@@ -9,8 +9,8 @@ function NegabaritnyeGruzoperevozki() {
     return (
         <>
             <Head>
-                <title>Transline</title>
-                <meta name="description" content="Перевозка негабаритного груза" />
+                <title>Перевозка негабаритного груза по Казахстану и СНГ</title>
+                <meta name="description" content="Специализированная логистическая компания предлагает профессиональные решения для перевозки негабаритных грузов в Казахстане и СНГ. Наш опыт и экспертиза позволяют эффективно организовать перевозку грузов любых размеров и характеристик, обеспечивая их безопасность и надежность доставки." />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
