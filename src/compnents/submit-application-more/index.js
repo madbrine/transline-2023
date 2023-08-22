de64898a2472e84951ab7785f81ec4997f49e957
Deduh <deduh@email.com>
2023-08-22 17:44:39 +0600

    upd

--- src/compnents/submit-application-more/index.js
+++ src/compnents/submit-application-more/index.js
@@ -66,7 +66,7 @@ function CoSubmitApplicationMore(props) {
             };
 
             // Отправка данных формы на сервер
-            await axios.post('https://185.234.114.179:4444/submit-form-more', allData);
+            await axios.post('https://transline.kz:4444/submit-form-more', allData);
 
             router.push('/spasibo');
         } catch (error) {
