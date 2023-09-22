8457c2d84f2d0abfab10820fd0c5a573c93d38aa
Deduh <deduh@email.com>
2023-09-22 12:33:00 +0600

    Update index.js

--- src/compnents/add-block-services/index.js
+++ src/compnents/add-block-services/index.js
@@ -9,6 +9,7 @@ import photo4 from '../../assets/services/train-cargo.webp';
 import photo5 from '../../assets/services/negabarit-cargo.webp';
 import photo6 from '../../assets/services/spec-tekhnika.webp';
 import Link from "next/link";
+import { useState, useRef } from "react";
 
 const inter = Inter({
     subsets: ['latin'],
@@ -16,10 +17,47 @@ const inter = Inter({
 })
 
 export default function CoBlockServices() {
+    const containerRef = useRef(null);
+    const [isDragging, setDragging] = useState(false);
+    const [startX, setStartX] = useState(null);
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+    const handleMouseDown = (e) => {
+        e.preventDefault();
+        if (!isDragging) {
+            setDragging(true);
+            setStartX(e.pageX);
+        }
+    };
+
+    const handleMouseUp = () => {
+        if (isDragging) {
+            setDragging(false);
+            setPrevScrollPos(containerRef.current.scrollLeft);
+        }
+    };
+
+    const handleMouseMove = (e) => {
+        if (isDragging) {
+            const x = e.pageX - startX;
+            containerRef.current.scrollLeft = prevScrollPos - x;
+        }
+    };
+
+
     return (
         <div className={inter.className}>
-            <div className={s['block-services']}>
-                <Link className={s['service']} id={s['margin-left']} href="/avtomobilnye-gruzoperevozki">
+            <div
+                ref={containerRef}
+                className={s['block-services']}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+                onMouseMove={handleMouseMove}
+                style={{
+                    cursor: isDragging ? 'grabbing' : 'grab',
+                }}
+            >
+                <div className={s['service']} id={s['margin-left']}>
                     <div className={s['service-image']}>
                         <Image src={photo1}
                             objectFit="cover"
@@ -32,26 +70,26 @@ export default function CoBlockServices() {
                             <div className={s['h2']}>Автомобильные перевозки</div>
                             <p>Наша глобальная сеть обеспечивает доставку грузов наших клиентов по всему миру</p>
                         </div>
-                        <a>Узнать больше</a>
+                        <a href="/avtomobilnye-gruzoperevozki">Узнать больше</a>
                     </div>
-                </Link>
-                <Link className={s['service']}  href="/multimodalnye-gruzoperevozki">
-                        <div className={s['service-image']}>
-                            <Image src={photo2}
-                                objectFit="cover"
-                                layout="fill"
-                                style={{ borderRadius: '20px' }}
-                            />
+                </div>
+                <div className={s['service']}>
+                    <div className={s['service-image']}>
+                        <Image src={photo2}
+                            objectFit="cover"
+                            layout="fill"
+                            style={{ borderRadius: '20px' }}
+                        />
+                    </div>
+                    <div className={s['text']}>
+                        <div>
+                            <div className={s['h2']}>Мультимодальные перевозки</div>
+                            <p>Позвольте нам помочь вам донести ваши продукты и идеи до всего мира</p>
                         </div>
-                        <div className={s['text']}>
-                            <div>
-                                <div className={s['h2']}>Мультимодальные перевозки</div>
-                                <p>Позвольте нам помочь вам донести ваши продукты и идеи до всего мира</p>
-                            </div>
-                            <a>Узнать больше</a>
-                        </div>
-                </Link>
-                <Link className={s['service']} href="/aviaperevozki">
+                        <a href="/multimodalnye-gruzoperevozki">Узнать больше</a>
+                    </div>
+                </div>
+                <div className={s['service']}>
                     <div className={s['service-image']}>
                         <Image src={photo3}
                             objectFit="cover"
@@ -64,10 +102,10 @@ export default function CoBlockServices() {
                             <div className={s['h2']}>Авиа перевозки</div>
                             <p>Мы обладаем знаниями, необходимыми для обслуживания широкого спектра отраслей</p>
                         </div>
-                        <a>Узнать больше</a>
+                        <a href="/aviaperevozki">Узнать больше</a>
                     </div>
-                </Link>
-                <Link className={s['service']} href="/zheleznodorozhnye-perevozki">
+                </div>
+                <div className={s['service']}>
                     <div className={s['service-image']}>
                         <Image src={photo4}
                             objectFit="cover"
@@ -80,10 +118,10 @@ export default function CoBlockServices() {
                             <div className={s['h2']}>Железнодорожные перевозки</div>
                             <p>Воспользуйтесь нашей железнодорожной сетью, чтобы соединить вашу цепочку поставок с основными экономическими регионами по всему миру.</p>
                         </div>
-                        <a>Узнать больше</a>
+                        <a href="/zheleznodorozhnye-perevozki">Узнать больше</a>
                     </div>
-                </Link>
-                <Link className={s['service']} href="perevozka-negabaritnyh-gruzov">
+                </div>
+                <div className={s['service']}>
                     <div className={s['service-image']}>
                         <Image src={photo5}
                             objectFit="cover"
@@ -96,10 +134,10 @@ export default function CoBlockServices() {
                             <div className={s['h2']}>Негабаритные перевозки</div>
                             <p>Мы осуществляем перевозки негабаритных грузов морским, железнодорожным и автомобильным транспортом. </p>
                         </div>
-                        <a>Узнать больше</a>
+                        <a href="perevozka-negabaritnyh-gruzov">Узнать больше</a>
                     </div>
-                </Link>
-                <Link className={s['service']} id={s['margin-right']} href="/arenda-specztehniki">
+                </div>
+                <div className={s['service']} id={s['margin-right']}>
                     <div className={s['service-image']}>
                         <Image src={photo6}
                             objectFit="cover"
@@ -112,29 +150,9 @@ export default function CoBlockServices() {
                             <div className={s['h2']}>Аренда спец. техники</div>
                             <p>У нас вы найдете самосвалы различных марок, моделей и грузоподъемности. </p>
                         </div>
-                        <a>Узнать больше</a>
-                    </div>
-                </Link>
-                {/* <div className={s['service']}>
-                    <img src="/services/tamoj-service.jpeg" />
-                    <div className={s['text']}>
-                        <div>
-                            <h2>Таможенное оформление</h2>
-                            <p>Pariatur eiusmod proident eu reprehenderit pariatur sit mollit aliquip pariatur laborum tempor reprehenderit.</p>
-                        </div>
-                        <a>Узнать больше</a>
+                        <a href="/arenda-specztehniki">Узнать больше</a>
                     </div>
                 </div>
-                <div className={s['service']} id={s['margin-right']}>
-                    <img src="/services/sklad-service.jpeg" />
-                    <div className={s['text']}>
-                        <div>
-                            <h2>Складские услуги</h2>
-                            <p>Pariatur eiusmod proident eu reprehenderit pariatur sit mollit aliquip pariatur laborum tempor reprehenderit.</p>
-                        </div>
-                        <a>Узнать больше</a>
-                    </div>
-                </div> */}
             </div>
         </div>
     )
